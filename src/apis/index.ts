9a28671841d010bd885ae87f799218c12ea12d4e
hanaminhtran <vnpalm@gmail.com>
2021-09-19 17:12:22 +0700

    make it better

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const urlOTP = "https://tiemchungcovid19.gov.vn/api/vaccination/public/otp-search?fullname=";
+const urlOTP = "/api/vaccination/public/otp-search?fullname=";
     //url = url +name + "&birthday=" + date.toString() + '&genderId=1&personalPhoneNumber=0919323236&identification=&healthInsuranceNumber';
 
-const urlGetData = 'https://tiemchungcovid19.gov.vn/api/vaccination/public/patient-vaccinated?fullname='
+const urlGetData = '/api/vaccination/public/patient-vaccinated?fullname='
 export const SendOTP  = (fullname, birthday,phone) => axios.get(
     `${urlOTP}${fullname}&birthday=${birthday}000&genderId=1&personalPhoneNumber=${phone}&identification=&healthInsuranceNumber`);
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import express from 'express';
+import { createProxyMiddleware, Filter, Options, RequestHandler } from 'http-proxy-middleware';
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
